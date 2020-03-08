617e07b0aa13fcb32182aa714bf5e96ed8af6e51
Kevin Prunty <kevin.prunty95@gmail.com>
2020-03-07 20:58:39 -0500

    Commented out references to keyv

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const Keyv = require ('keyv');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-const keyv = new Keyv(uri);
-keyv.on('error', err => console.error('Keyv connection error:', err));
+// const keyv = new Keyv(uri);
+//keyv.on('error', err => console.error('Keyv connection error:', err));
 
 
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles){
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command)
 }
 
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 client.once('ready', () => {
 	console.log('Ready!');
 });
